8cd11b8d2ec11ec5ad4d9063b573bb2b9bae8b29
Kyle Ellrott <kellrott@gmail.com>
2016-08-10 14:03:13 -0700

    Doing workflow packing before loading

--- load_jobs.py
+++ load_jobs.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 
 import argparser
+import subprocess
 from kafka import KafkaProducer
 
+
+def which(file):
+    for path in os.environ["PATH"].split(":"):
+        p = os.path.join(path, file)
+        if os.path.exists(p):
+            return p
 
 if __name__ == "__main__":
     parser = argparser.ArgumentParser()
@@ -17,18 +24,19 @@ if __name__ == "__main__":
     producer = KafkaProducer(bootstrap_servers=args.server)
 
     with open(args.workflow) as handle:
-        workflow = yaml.load(handle.read())
+        proc = subprocess.call([which('cwltool'), '--pack', args.workflow], stdout=subprocess.PIPE)
+        stdout, stderr = proc.communicate()
+        workflow = json.loads(stdout)
 
     with open(args.inputs) as handle:
         for line in handle:
             inputs = json.loads(line)
-            
             output = os.path.join( args.output, inputs[args.id_field] )
-            
             data = {
                 "workflow" : workflow,
                 "inputs" : inputs,
                 "output" : output
             }            
-
             producer.send('cwl-jobs', json.dumps(data))
+
+
